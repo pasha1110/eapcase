d29cd412b7b090b620d9554b62fbabaecb5e7760
erlangga <erlanggaabdeepasha@gmail.com>
2021-03-02 13:21:07 +0700

    add new content in readme.md

---
+++ node_modules/eapcase/test.js
@@ -0,0 +1,10 @@
+const Case = require("./index");
+
+console.log(Case.kebab("Hello World"));
+console.log(Case.snake("Hello World"));
+console.log(Case.camel("Hello World"));
+console.log(Case.pascal("Hello World"));
+console.log(Case.lower("halo dunia"));
+console.log(Case.upper("halo dunia"));
+console.log(Case.noWhiteSpace("halo dunia"));
+console.log(Case.path("halo dunia"));
